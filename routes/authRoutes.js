46f48afac8ffd9f3ab3f0b0a3b8f28db5e95ecb6
João Victor Silva <joaovictordes02@gmail.com>
2024-08-28 02:45:30 -0300

    flash messages

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const AuthController = require("../controllers/AuthController");
 
 router.get("/login", AuthController.login);
-
 router.get("/register", AuthController.register);
+router.post("/register", AuthController.registerPost);
 
 module.exports = router;
