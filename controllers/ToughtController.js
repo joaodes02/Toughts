47d95c0972423dd07000069bf3b4452a5efd6b6e
João Victor Silva <joaovictordes02@gmail.com>
2024-08-29 16:20:22 -0300

    Autalizando dados

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -69,4 +69,31 @@ module.exports = class ToughtController {
       console.log(error);
     }
   }
+
+  static async updateTought(req, res) {
+    const id = req.params.id;
+
+    const tought = await Tought.findOne({ where: { id: id }, raw: true });
+
+    res.render("toughts/edit", { tought });
+  }
+
+  static async updateToughtSave(req, res) {
+    const id = req.body.id;
+
+    const tought = {
+      title: req.body.title,
+    };
+
+    try {
+      await Tought.update(tought, { where: { id: id } });
+      req.flash("message", "Pensamento atualizado!");
+
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -9,6 +9,8 @@ const checkAuth = require("../helpers/auth").checkAuth;
 // controller
 router.post("/add", checkAuth, ToughtController.createToughtSave);
 router.get("/add", checkAuth, ToughtController.createTought);
+router.get("/edit/:id", checkAuth, ToughtController.updateTought);
+router.post("/edit", checkAuth, ToughtController.updateToughtSave);
 router.get("/dashboard", checkAuth, ToughtController.dashboard);
 router.post("/remove", checkAuth, ToughtController.removeTought);
 router.get("/", ToughtController.showToughts);
