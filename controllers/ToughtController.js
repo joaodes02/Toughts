ebe1468bd7ac3109d7ff668d2b37d41d7289ef23
João Victor Silva <joaovictordes02@gmail.com>
2024-08-29 12:27:28 -0300

    Mensagem de dashboard vazio

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -23,9 +23,14 @@ module.exports = class ToughtController {
 
     const toughts = user.Toughts.map((result) => result.dataValues);
 
+    let emptyToughts = false;
+    if (toughts.length === 0) {
+      emptyToughts = true;
+    }
+
     // console.log(toughts);
 
-    res.render("toughts/dashboard", { toughts });
+    res.render("toughts/dashboard", { toughts, emptyToughts });
   }
 
   static createTought(req, res) {
